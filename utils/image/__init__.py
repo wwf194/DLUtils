b813cf5b290837fb16404d3eb8f2b753f1c34f98
timwang <weifanwang16@gmail.com>
2023-05-19 09:42:03 +0900

    update

--- utils/image/__init__.py
+++ utils/image/__init__.py
@@ -2,6 +2,8 @@ import DLUtils.utils.image.compress as compress
 import DLUtils
 import cv2
 import numpy as np
+import DLUtils.utils.image.compress as compress
+from .compress import File2Image, Image2File, ImageNp2File, ResizeImage
 
 def GenerateBlackImage(Height=512, Width=512, ChannelNum=3):
     assert 1<= ChannelNum <= 4
@@ -14,10 +16,6 @@ def GenerateBlackImageInt16(Height=512, Width=512, ChannelNum=3):
 
 def GenerateSingleColorImage(Height, Width, Color, ChannelNum, DataType):
     DataType = DLUtils.ParseDataTypeNp(DataType)
-
-import DLUtils.utils.image.compress as compress
-from .compress import File2Image, Image2File, ImageNp2File, ResizeImage
-
 
 def TextOnImage(Image):
     cv2.PutText()
